51aa1aa70b78381b9550f409c9b9e3f7355cc664
Amir Hamza <hamza.com.bd@gmail.com>
2024-03-21 19:42:00 +0600

    data fetched from api and checked

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(
+    private _http: HttpClient,
+
+  ){}
+
+  ngOnInit(): void {
+    this.loadTasks()
+  }
+
+  getTasks(): Observable<any[]> {
+    return this._http.get<any[]>('http://localhost:3000/tasks');
+  }
+
+  loadTasks(): void {
+    this.getTasks().subscribe((res: any[])=>{
+      console.log(res)
+    });
+  }
 }
+
